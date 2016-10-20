09cdf625e904cfcb653f3ba9040ed467fa28fb78
Kevin Paul Kalis <kevinpaulkalis@gmail.com>
2016-10-20 10:02:59 +0800

    Create manager for Account model

--- booktrade/authentication/models.py
+++ booktrade/authentication/models.py
@@ -2,6 +2,36 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+
+# Account model's manager
+class AccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Users must have a valid email address')
+
+        if not kwargs.get('username'):
+            raise ValueError('Users must have a valid username')
+
+        account = self.model(
+            email=self.normalize_email(email),
+            username=kwargs.get('username')
+            )
+
+        account.set_password(password)
+        account.save()
+
+        return account
+
+    def create_superuser(self, email, password, **kwargs):
+        account = self.create_user(email, password, **kwargs)
+
+        account.is_admin = True
+        account.save()
+
+        return account
+
+# Our Account model for logging-in
 class Account(AbstractBaseUser):
     email = models.EmailField(unique=True)
     username = models.CharField(unique=True, max_length=50)
